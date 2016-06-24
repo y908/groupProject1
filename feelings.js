3a079a59f2a20d3e08419e18736138271571ec0c
Yana Yakubchik <Yana Yakubchik>
2016-06-24 17:46:23 -0400

    created modal alert, added new gifs, and cleaned up code

--- feelings.js
+++ feelings.js
@@ -1,3 +1,32 @@
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+
 // global variables
 
 var perPage = 3;
@@ -86,7 +115,7 @@ function initMap() {
     //                  MARKER ON THE MAP
     //==================================================
 
-    var imageFlag = ['pbjelly.gif','rooster.gif','dino.gif','beachflag.png','rabbit.gif'];
+    var imageFlag = ['pbjelly.gif','rooster.gif','dino.gif','rabbit.gif', 'datboi.gif', 'pokemon.gif', 'heart.gif', 'what.gif'];
 
     // so when you click it puts a pin there 
     // EVENT LISTENER - click on map
@@ -98,12 +127,15 @@ function initMap() {
       perPage = document.getElementById("imgAppear").value;
 
       if (isNaN(perPage)){
-        alert("lol");
+
+        $('#errorMsg').html("That's not a number!");
+        modal.style.display = "block";
 
       }
-      else if (perPage > 20){
+      else if (perPage > 20 || perPage < 0){
 
-        alert("That's too many pictures to load!");
+        $('#errorMsg').html("Please enter a # between 0 and 20. :)");
+        modal.style.display = "block";
       }
       else{
 
