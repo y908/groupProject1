b0fe85891e591300b4877a0685b5f0ebe04a8440
Yana Yakubchik <Yana Yakubchik>
2016-06-24 14:51:53 -0400

    made new comments, cleaned code, created better way to pull photos, created input

--- feelings.js
+++ feelings.js
@@ -1,32 +1,19 @@
+// global variables
+
+var perPage = 3;
+
+var radius = 5;
 
 
 
 // -----------------------
-//	flickr stuff
+//  flickr stuff
 // -----------------------
 
-// use YOUR OWN KEY!!!!
-// needs to be updated
 var apiKey = "87d4ef71bdff0695666b4d895cb3b651";
-	
-
-// create variables
-// for flickr
-var apiurl;
-var apiurl_size;
-var selected_size;  
-
-// set size
-// of image to download
-selected_size=75; 
- 
-
-// give lat & long and it will show within 20 mile radius
-// apiurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&has_geo=1&lat=40.37&lon=-74.299&radius=20&api_key=" + apiKey  + "&per_page=10&format=json&nojsoncallback=1";  
-
-
+   
 // -----------------------
-//	GOOGLE STUFF
+//  GOOGLE STUFF
 // -----------------------
 
 // starting location
@@ -37,250 +24,130 @@ var temp2 = -75;
 // initiate map
 function initMap() {
 
-
-  // latitude and logitude
-  var myLatLng = {lat: temp1, lng: temp2};
-
-
-	// old code to start a map
-
-	/*
-
-	  // create new map
-	  var map = new google.maps.Map(document.getElementById('map'), {
-		  zoom: 8,
-		  center: myLatLng
-		});
-
-	*/
+    // latitude and logitude
+    var myLatLng = {lat: temp1, lng: temp2};
 
 
+    //==========================================================
+    //                          style map our way
+    //==========================================================
+    var styles = [
+        {
+          stylers: [
+          { "saturation": -40 },
+          { "hue": "#0008ff" },
+          { "visibility": "simplified" }
+          ]
+        },{
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [
+            { lightness: 50 },
+            { visibility: "simplified" }
+          ]
+        },{
+          featureType: "road",
+          elementType: "labels",
+          stylers: [
+            { visibility: "off" }
+          ]
+        },{
+          featureType: "labels",
+          elementType: "labels",
+          stylers: [
+            { lightness: 40 }
+          ]
+        }
+      ];
 
-// style map our way
-var styles = [
-    {
-      stylers: [
-      { "saturation": -40 },
-      { "hue": "#0008ff" },
-      { "visibility": "simplified" }
-      ]
-    },{
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [
-        { lightness: 50 },
-        { visibility: "simplified" }
-      ]
-    },{
-      featureType: "road",
-      elementType: "labels",
-      stylers: [
-        { visibility: "off" }
-      ]
-    },{
-      featureType: "labels",
-      elementType: "labels",
-      stylers: [
-        { lightness: 40 }
-      ]
-    }
-  ];
-
-var styledMap = new google.maps.StyledMapType(styles,
-    {name: "Styled Map"});
-
-
-var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(temp1, temp2),
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-    }
-  };
+    var styledMap = new google.maps.StyledMapType(styles,
+        {name: "Styled Map"});
 
 
-  var map = new google.maps.Map(document.getElementById('map'),
-    mapOptions);
-
-  //Associate the styled map with the MapTypeId and set it to display.
-  map.mapTypes.set('map_style', styledMap);
-  map.setMapTypeId('map_style');
-
-// end of styling map
-
-
-// marker image
-//var imageFlag = 'beachflag.png';
-var imageFlag = 'pbjelly.gif';
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(temp1, temp2),
+        mapTypeControlOptions: {
+          mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+        }
+      };
 
 
-// so when you click it puts a pin there 
-// EVENT LISTENER - click on map
-// here!!!
-google.maps.event.addListener(map, 'click', function(eventFTW) {
+      var map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
 
-    marker = new google.maps.Marker({
-    	position: eventFTW.latLng, 
-    	map: map,
-   	  icon: imageFlag,
- 	    optimized: false,
-    });
+      //Associate the styled map with the MapTypeId and set it to display.
+      map.mapTypes.set('map_style', styledMap);
+      map.setMapTypeId('map_style');
+
+    // end of styling map
+
+    //==================================================
+    //                  MARKER ON THE MAP
+    //==================================================
+
+    var imageFlag = ['pbjelly.gif','rooster.gif','dino.gif','beachflag.png','rabbit.gif'];
+
+    // so when you click it puts a pin there 
+    // EVENT LISTENER - click on map
+    // here!!!
+    google.maps.event.addListener(map, 'click', function(eventFTW) {
+
+       marker = new google.maps.Marker({
+         position: eventFTW.latLng, 
+         map: map,
+         animation: google.maps.Animation.DROP,
+         icon: imageFlag[Math.floor(imageFlag.length * Math.random())],
+         optimized: false,
+       });
+
+      // perPage = 3;
+      perPage = document.getElementById("imgAppear").value;
+      
+     //get photos from location where I clicked
+
+       loadPhotos(eventFTW.latLng.lat(),eventFTW.latLng.lng());
 
 
-	//get photos from location where I clicked
-
-    loadPhotos(eventFTW.latLng.lat(),eventFTW.latLng.lng());
-
-}); // end map click listener
+    }); // end map click listener
 
 
+//=============================================================  
+}//          END OF GOOGLE MAPS INITIATION FUNCTION
+//=============================================================
 
 
-//==========================================
-// MARKERS ON GOOGLE MAPS
-//==========================================
-
-
-// // starting marker
-//   var marker = new google.maps.Marker({
-//     position: myLatLng,
-//     map: map,
-//     icon: imageFlag,
-//     optimized: false,
-//     title: 'hello world'
-//   });
+//===============================================================
+//                flickr function for GPS input
+//===============================================================
 
 
 
-// // add another marker  -- this code works
-// myLatlng = new google.maps.LatLng(temp1+1,temp2+1);
-
-// 	var marker = new google.maps.Marker({
-// 	    position: myLatlng,
-// 	    title:"Hello World!"
-// 	});
-
-// 	// To add the marker to the map, call setMap();
-// 	marker.setMap(map);
-  
-}//END OF GOOGLE MAPS INITIATION FUNCTION
-//========================================
-// END OF GOOGLE MAPS INITIATION FUNCTION
-//========================================
-
-
-
-
-//======================================
-// code I found -- doesn't work yet
-//======================================
-
-// function addMyMarker() { //function that will add markers on button click
-
-// 	alert("hello");
-
-// myLatlng = new google.maps.LatLng(temp1+1,temp2+1);
-
-
-//     var marker = new google.maps.Marker({
-//         position:myLatlng,
-//         map: map,
-//         title:"This a new marker!"
-//     });
-
-//     marker.setMap(map);
-// }
-
-//======================================
-// trying to get it work after initialization
-// doesn't work yet!
-//=======================================
-
-
-// function placemarker(){
-
-//       temp1 = 41.5;
-//       temp2 = -74.5;
-
-// // add another marker
-//   myLatLng2 = {lat: temp1, lng: temp2};
-
-//   marker = new google.maps.Marker({
-//     position: myLatLng2,
-//     map: map,
-//     title: 'Hello World 2!'
-//   });
-//   // google maps marker
-
-// }
-
-
-
-
-
-
-
-//===========================================================================
-// 		flickr function for GPS input
-//===========================================================================
 
 
 function loadPhotos(aaa,bbb){  
 
-	apiurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&has_geo=1&lat="+aaa+"&lon="+bbb+"&radius=5&api_key=" + apiKey  + "&per_page=3&format=json&nojsoncallback=1";  
+  var apiurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&has_geo=1&lat="+aaa+"&lon="+bbb+"&radius=" + radius + "&api_key=" + apiKey  + "&extras=geo&per_page=" + perPage + "&format=json&nojsoncallback=1";  
 
-	$.getJSON(apiurl,function(jsonFTW){  
+  $.getJSON(apiurl,function(jsonFTW){  
 
-		//console.log(jsonFTW);
-
-		$.each(jsonFTW.photos.photo,function(i,myresult){  
-
-			//console.log("running" + i);
-			
-			// my result.id --- the ID can be for a specific photo, eg: 27234882763
-
-			apiurl_size = "https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=" + apiKey + "&photo_id="+myresult.id+"&format=json&nojsoncallback=1"; 
-
-			$.getJSON(apiurl_size,function(sizeFTW){  
-
-				//console.log(sizeFTW);
-
-				// only display stuff if we know its GPS location
-				if(sizeFTW.photo.location.latitude != ""){
-
-					//alert("success");
-
-					apiurl_size = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=" + apiKey + "&photo_id="+myresult.id+"&format=json&nojsoncallback=1"; 
-
-					$.getJSON(apiurl_size,function(sizeFTW2){  
-
-						$.each(sizeFTW2.sizes.size,function(i,myresult_size){  
+    //console.log(jsonFTW);
+    
+    for (i=0; i<perPage; i++){ 
+      var imageURL = "https://farm"+jsonFTW.photos.photo[i].farm+".staticflickr.com/"+jsonFTW.photos.photo[i].server+"/"+jsonFTW.photos.photo[i].id+"_"+jsonFTW.photos.photo[i].secret+".jpg";
+      
+      var imageURLsmall = "https://farm"+jsonFTW.photos.photo[i].farm+".staticflickr.com/"+jsonFTW.photos.photo[i].server+"/"+jsonFTW.photos.photo[i].id+"_"+jsonFTW.photos.photo[i].secret+"_s.jpg";
 
 
-							//console.log("doing" + i);
+      //console.log("lat = " + jsonFTW.photos.photo[i].latitude);
+      //console.log("lon = " + jsonFTW.photos.photo[i].longitude);   
 
-							// this only shows the images of the correct size !!!
-							// otherwise ALL size types of each image is displayed !!!
+    $("#results").prepend('<a href="'+ imageURL +'" target="_blank"><img title="'+jsonFTW.photos.photo[i].latitude+' and '+jsonFTW.photos.photo[i].longitude+'" src="'+imageURLsmall+'"/></a>');
 
-							// only load SQUARE previews -- 75x75
-							if(myresult_size.width==selected_size && myresult_size.height==selected_size){ 
-								// displays longitude too 
-								//$("#results").append('<a href="'+myresult_size.url+'" target="_blank"><img src="'+myresult_size.source+'"/></a><br><text>Latitude:'+sizeFTW.photo.location.latitude+'<br>Longitude:'+sizeFTW.photo.location.longitude+'</text><br>');  
-								$("#results").append('<a href="'+myresult_size.url+'" target="_blank"><img title="'+sizeFTW.photo.location.latitude+' and '+sizeFTW.photo.location.longitude+'" src="'+myresult_size.source+'"/></a>');
 
-							} // end of IF statement
+    }    
 
-						}); // end of .each
-
-					}) // end of .getJSON
-
-				} // end of IF statement
-
-			}) // end of .getJSON
-
-		}); // end of .each
-
-	}); // end of .getJSON
+  }); // end of .getJSON
 
 } // end loadPhotos
 
@@ -291,25 +158,21 @@ $(document).ready(function() {
 
 // code to have the button do something -- still doesn't work
 
-/*	 $('#btn').on('click', function(){
-	 	//placemarker(41.2,-74.2);
-	 	alert("you clicked");
+  $('#btn').on('click', function(){
+    perPage = document.getElementById("imgAppear").value;
+    radius = document.getElementById("miles").value;
 
-	 });*/
+   });
 
-
-	/*$('#btn').on('click', function() {
-
-		//alert("hi");
-
-	      temp1 = 41.5;
-	      temp2 = -74.5;
-		  myLatLng2 = {lat: temp1, lng: temp2};
-
-	//    addmarker(myLatLng2);
-	//  		addMyMarker();
-
-	})*/
+/*
+  /*$('#btn').on('click', function() {
+    //alert("hi");
+        temp1 = 41.5;
+        temp2 = -74.5;
+      myLatLng2 = {lat: temp1, lng: temp2};
+  //    addmarker(myLatLng2);
+  //      addMyMarker();
+  })*/
 
 // end of *** code
 
